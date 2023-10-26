0f8f0d786f203b91fdc1462c8ac06f4cc2695505
HoKhoa123 <hodoanminhkhoa.9a1@gmail.com>
2023-10-26 12:25:38 +0700

    BE UpdateStatusProcessController with checkUpdate

--- Fall2023_SWP391_NET1704_Vitvang/src/main/java/com/vitvang/productionmanagement/controller/UpdateStatusProcessController.java
+++ Fall2023_SWP391_NET1704_Vitvang/src/main/java/com/vitvang/productionmanagement/controller/UpdateStatusProcessController.java
@@ -5,6 +5,7 @@
 package com.vitvang.productionmanagement.controller;
 
 import com.vitvang.productionmanagement.dao.process.ProcessDAO;
+import com.vitvang.productionmanagement.model.ProcessDTO;
 import java.io.IOException;
 import java.io.PrintWriter;
 import jakarta.servlet.ServletException;
@@ -12,7 +13,10 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import static java.lang.System.out;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -26,15 +30,52 @@ public class UpdateStatusProcessController extends HttpServlet {
       protected void processRequest(HttpServletRequest request, HttpServletResponse response)
               throws ServletException, IOException, SQLException {
             response.setContentType("text/html;charset=UTF-8");
+            String Status = request.getParameter("txtStatus");
             String ProcessID = request.getParameter("txtProcessID");
             String OrderID = request.getParameter("txtOrderID");
             String CageID = request.getParameter("txtCageID");
-            String NumberOfEmployee = request.getParameter("txtNumberOfEmployee");
-            String Status = request.getParameter("txtStatus");
+//            String NumberOfEmployee = request.getParameter("txtNumberOfEmployee");
             String url = "NewLogin.jsp";
             try {
-                  ProcessDAO processdao = new ProcessDAO();
-                  boolean result = processdao.updateStatusProcess(Status, ProcessID, OrderID, CageID);
+                  boolean result = false;
+                  int count = 0;
+                  ProcessDAO dao = new ProcessDAO();
+//                  if (ProcessID.equals("PR001") && Status.equals("Done")) {
+//                        result = dao.updateStatusProcess1(Status, ProcessID, OrderID, CageID);
+//                  } else if (ProcessID.equals("PR002") && Status.equals("Done")) {
+//                        result = dao.updateStatusProcess2(Status, ProcessID, OrderID, CageID);
+//                  } else if (ProcessID.equals("PR003") && Status.equals("Done")) {
+//                        result = dao.updateStatusProcess3(Status, ProcessID, OrderID, CageID);
+//                  } else if (ProcessID.equals("PR004") && Status.equals("Done")) {
+//                        result = dao.updateStatusProcess4(Status, ProcessID, OrderID, CageID);
+//                  } else if (ProcessID.equals("PR005") && Status.equals("Done")) {
+//                        result = dao.updateStatusProcess5(Status, ProcessID, OrderID, CageID);
+//                  } else {
+//                        result = dao.updateStatusProcess(Status, ProcessID, OrderID, CageID);
+//                  }
+                  dao.ViewProcessingOrder(OrderID, CageID, CageID);
+                  List<ProcessDTO> list = dao.getListOrdersProcess();
+                  for (ProcessDTO process : list) {
+                        count++;
+
+                        if (process.getProcessID().equals(ProcessID) && !Status.equals("Done")) {
+                              result = dao.updateStatusProcess(Status, ProcessID, OrderID, CageID);
+                              break;
+                        }
+
+                        if (process.getProcessID().equals(ProcessID) && Status.equals("Done") && count != list.size()) {
+//                              request.setAttribute("UPDATE_STATUS", ProcessID);
+                              String nextProcessID = getNextProcessID(ProcessID);
+                              result = dao.updateStatusProcessToDone(Status, ProcessID, nextProcessID, OrderID, CageID);
+                              break;
+                        }
+
+                        if (process.getProcessID().equals(ProcessID) && Status.equals("Done") && count == list.size()) {
+                              result = dao.updateStatusLastProcess(Status, ProcessID, OrderID, CageID);
+                              break;
+                        }
+                  }
+
                   if (result) {
                         url = "MainController?btAction=ViewProcessDetail";
                   }
@@ -43,6 +84,14 @@ public class UpdateStatusProcessController extends HttpServlet {
             } finally {
                   request.getRequestDispatcher(url).forward(request, response);
             }
+      }
+
+      private static String getNextProcessID(String processID) {
+            String prefix = processID.substring(0, 2); // Lấy phần tiền tố "PR"
+            int number = Integer.parseInt(processID.substring(2)); // Lấy phần số xxx
+            int nextNumber = number + 1; // Tăng giá trị số xxx lên 1
+            String nextNumberString = String.format("%03d", nextNumber); // Định dạng lại số xxx thành chuỗi có 3 chữ số
+            return prefix + nextNumberString; // Tạo chuỗi processID mới bằng cách kết hợp phần tiền tố và phần số
       }
 
       // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
